4f4711d1bf8142aa0631597a5c1d3b75f0b4ed8d
Terrell Gu <terrell.gu@lightnet.io>
2021-03-30 15:36:02 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'core-js/stable'
+import Vue from 'vue'
+//import CoreuiVuePro from '@coreui/vue-pro'
+import CoreuiVuePro from '../node_modules/@coreui/vue-pro/src/index.js'
+import App from './App'
+import router from './router/index'
+import { iconsSet as icons } from './assets/icons/icons.js'
+import store from './store'
+import i18n from './i18n.js'
+
+Vue.use(CoreuiVuePro)
+Vue.prototype.$log = console.log.bind(console)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  //CIcon component documentation: https://coreui.io/vue/docs/components/icon
+  icons,
+  i18n,
+  template: '<App/>',
+  components: {
+    App
+  }
+})
