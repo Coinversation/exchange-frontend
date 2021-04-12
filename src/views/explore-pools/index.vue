927b47a4c6c245c1904a5d4c85e0af7a917a8912
谷磊 <gulei@guleideMacBook-Pro.local>
2021-04-12 16:02:45 +0800

    fix table

--- src/views/explore-pools/index.vue
+++ src/views/explore-pools/index.vue
@@ -1,104 +1,212 @@
+<script>
+// import MainChartExample from "../charts/MainChartExample";
+// import WidgetsBrand from "../widgets/WidgetsBrand";
+
+export default {
+	name: "Explore pools",
+	components: {},
+	data() {
+		return {
+			selected: "Month",
+			tableItems: [
+				{
+					// avatar: { url: "img/avatars/1.jpg", status: "success" },
+					user: {
+						name: "Yiorgos Avraamu",
+						new: true,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "USA", flag: "cif-us" },
+					usage: { value: 50, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "Mastercard", icon: "cib-cc-mastercard" },
+					activity: "10 sec ago",
+				},
+				{
+					avatar: { url: "img/avatars/2.jpg", status: "danger" },
+					user: {
+						name: "Avram Tarasios",
+						new: false,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "Brazil", flag: "cif-br" },
+					usage: { value: 22, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "Visa", icon: "cib-cc-visa" },
+					activity: "5 minutes ago",
+				},
+				{
+					avatar: { url: "img/avatars/3.jpg", status: "warning" },
+					user: {
+						name: "Quintin Ed",
+						new: true,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "India", flag: "cif-in" },
+					usage: { value: 74, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "Stripe", icon: "cib-stripe" },
+					activity: "1 hour ago",
+				},
+				{
+					avatar: { url: "img/avatars/4.jpg", status: "" },
+					user: {
+						name: "Enéas Kwadwo",
+						new: true,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "France", flag: "cif-fr" },
+					usage: { value: 98, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "PayPal", icon: "cib-paypal" },
+					activity: "Last month",
+				},
+				{
+					avatar: { url: "img/avatars/5.jpg", status: "success" },
+					user: {
+						name: "Agapetus Tadeáš",
+						new: true,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "Spain", flag: "cif-es" },
+					usage: { value: 22, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "Google Wallet", icon: "cib-google-pay" },
+					activity: "Last week",
+				},
+				{
+					avatar: { url: "img/avatars/6.jpg", status: "danger" },
+					user: {
+						name: "Friderik Dávid",
+						new: true,
+						registered: "Jan 1, 2015",
+					},
+					country: { name: "Poland", flag: "cif-pl" },
+					usage: { value: 43, period: "Jun 11, 2015 - Jul 10, 2015" },
+					payment: { name: "Amex", icon: "cib-cc-amex" },
+					activity: "Last week",
+				},
+			],
+			tableFields: [
+				// { key: "avatar", label: "", _classes: "text-center" },
+				{ key: "Pool address" },
+				{ key: "Assets", _classes: "text-center" },
+				{ key: "Swap fee" },
+				{ key: "Market cap" },
+				{ key: "My liquidity" },
+				{ key: "Volume (24h)" },
+				// {
+				// 	key: "payment",
+				// 	label: "Payment method",
+				// 	_classes: "text-center",
+				// },
+				// { key: "activity" },
+			],
+		};
+	},
+	methods: {
+		color(value) {
+			let $color;
+			if (value <= 25) {
+				$color = "info";
+			} else if (value > 25 && value <= 50) {
+				$color = "success";
+			} else if (value > 50 && value <= 75) {
+				$color = "warning";
+			} else if (value > 75 && value <= 100) {
+				$color = "danger";
+			}
+			return $color;
+		},
+	},
+};
+</script>
+
 <template>
 	<div>
 		<CRow>
-			<CCol lg="6">
-				<CTableWrapper :items="getShuffledUsersData()">
-					<template #header>
-						<CIcon name="cil-grid" /> Simple Table
-						<div class="card-header-actions">
-							<a
-								href="https://coreui.io/vue/docs/components/nav"
-								class="card-header-action"
-								rel="noreferrer noopener"
-								target="_blank"
+			<CCol md="12">
+				<CCard>
+					<!-- <CCardHeader> Traffic &amp; Sales </CCardHeader> -->
+					<CCardBody>
+						<CDataTable
+							class="mb-0 table-outline"
+							hover
+							:items="tableItems"
+							:fields="tableFields"
+							head-color="light"
+							no-sorting
+						>
+							<td
+								slot="avatar"
+								class="text-center"
+								slot-scope="{ item }"
 							>
-								<small class="text-muted">docs</small>
-							</a>
-						</div>
-					</template>
-				</CTableWrapper>
-			</CCol>
-
-			<CCol lg="6">
-				<CTableWrapper
-					:items="getShuffledUsersData()"
-					striped
-					caption="Striped Table"
-				/>
-			</CCol>
-		</CRow>
-
-		<CRow>
-			<CCol lg="6">
-				<CTableWrapper
-					:items="getShuffledUsersData()"
-					small
-					caption="Condensed Table"
-				/>
-			</CCol>
-
-			<CCol lg="6">
-				<CTableWrapper
-					:items="getShuffledUsersData()"
-					fixed
-					border
-					caption="Bordered Table"
-				/>
-			</CCol>
-		</CRow>
-
-		<CRow>
-			<CCol sm="12">
-				<CTableWrapper
-					:items="getShuffledUsersData()"
-					hover
-					striped
-					border
-					small
-					fixed
-					caption="Combined All Table"
-				/>
-			</CCol>
-		</CRow>
-
-		<CRow>
-			<CCol sm="12">
-				<CTableWrapper
-					:items="getShuffledUsersData()"
-					hover
-					striped
-					border
-					small
-					fixed
-					dark
-					caption="Combined All dark Table"
-				/>
+								<div class="c-avatar">
+									<img
+										:src="item.avatar.url"
+										class="c-avatar-img"
+										alt=""
+									/>
+									<span
+										class="c-avatar-status"
+										:class="`bg-${
+											item.avatar.status || 'secondary'
+										}`"
+									></span>
+								</div>
+							</td>
+							<td slot="Pool address" slot-scope="{ item }">
+								<div>{{ item.user.name }}</div>
+								<div class="small text-muted">
+									<span>
+										<template v-if="item.user.new"
+											>New</template
+										>
+										<template v-else>Recurring</template>
+									</span>
+									| Registered: {{ item.user.registered }}
+								</div>
+							</td>
+							<td
+								slot="Assets"
+								slot-scope="{ item }"
+								class="text-center"
+							>
+								<CIcon :name="item.country.flag" height="25" />
+							</td>
+							<td slot="usage" slot-scope="{ item }">
+								<div class="clearfix">
+									<div class="float-left">
+										<strong>{{ item.usage.value }}%</strong>
+									</div>
+									<div class="float-right">
+										<small class="text-muted">{{
+											item.usage.period
+										}}</small>
+									</div>
+								</div>
+								<CProgress
+									class="progress-xs"
+									v-model="item.usage.value"
+									:color="color(item.usage.value)"
+								/>
+							</td>
+							<td
+								slot="Swap fee"
+								slot-scope="{ item }"
+								class="text-center"
+							>
+								<CIcon :name="item.payment.icon" height="25" />
+							</td>
+							<td slot="Market cap" slot-scope="{ item }">
+								<div class="small text-muted">Last login</div>
+								<strong>{{ item.activity }}</strong>
+							</td>
+                            <td slot="My liquidity" slot-scope="{ item }">
+								<div class="small text-muted">Last login</div>
+								<strong>{{ item.activity }}</strong>
+							</td>
+						</CDataTable>
+					</CCardBody>
+				</CCard>
 			</CCol>
 		</CRow>
 	</div>
 </template>
 
-<script>
-import CTableWrapper from "../tables/Table";
-import usersData from "../users/UsersData";
-
-export default {
-	name: "Tables",
-	components: { CTableWrapper },
-	methods: {
-		shuffleArray(array) {
-			for (let i = array.length - 1; i > 0; i--) {
-				let j = Math.floor(Math.random() * (i + 1));
-				let temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-			}
-			return array;
-		},
-
-		getShuffledUsersData() {
-			return this.shuffleArray(usersData.slice(0));
-		},
-	},
-};
-</script>
