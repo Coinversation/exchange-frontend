cbf4ca353bbee4129af36a0ec7cbc5d1bcdd35b5
Terrell Gu <terrell.gu@lightnet.io>
2021-05-19 16:53:24 +0800

    keep js

--- vue.config.js
+++ vue.config.js
@@ -3,25 +3,11 @@ module.exports = {
     runtimeCompiler: true,
     configureWebpack: {
         //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
-        // resolve: {
-        //     symlinks: false,
-        // },
-        resolve: { extensions: ['.ts', '.tsx', '.js', '.json'] },
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    loader: 'ts-loader',
-                    exclude: /node_modules/,
-                    options: {
-                        appendTsSuffixTo: [/\.vue$/],
-                    },
-                },
-            ],
+        resolve: {
+            symlinks: false,
         },
     },
     transpileDependencies: ['@coreui/utils'],
-
     // use this option for production linking
     // publicPath: process.env.NODE_ENV === 'production' ? '/vue/demo/3.1.0' : '/'
 }
